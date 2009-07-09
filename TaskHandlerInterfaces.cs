a60241d01f3830427febd4b8a22084f5e2787fc4
dahall_cp <SND\dahall_cp>
2009-07-09 04:14:58 +0000

    Fixed bug with running tasks enumeration; removed ITaskHandler and ITaskHandlerStatus to separate file and made interfaces public; Added project to show how to create a COM task; Updated test harness

---
+++ TaskHandlerInterfaces.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Win32.TaskScheduler
+{
+	[ComImport, Guid("839D7762-5121-4009-9234-4F0D19394F04"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown), System.Security.SuppressUnmanagedCodeSecurity]
+	public interface ITaskHandler
+	{
+		void Start([In, MarshalAs(UnmanagedType.IUnknown)] object pHandlerServices, [In, MarshalAs(UnmanagedType.BStr)] string Data);
+		void Stop([MarshalAs(UnmanagedType.Error)] out int pRetCode);
+		void Pause();
+		void Resume();
+	}
+
+	[ComImport, Guid("EAEC7A8F-27A0-4DDC-8675-14726A01A38A"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown),
+	System.Security.SuppressUnmanagedCodeSecurity]
+	public interface ITaskHandlerStatus
+	{
+		void UpdateStatus([In] short percentComplete, [In, MarshalAs(UnmanagedType.BStr)] string statusMessage);
+		void TaskCompleted([In, MarshalAs(UnmanagedType.Error)] int taskErrCode);
+	}
+}
