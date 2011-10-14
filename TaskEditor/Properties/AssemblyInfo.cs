28b905c8867d752ca483573edb0d85dbb1858fe6
dahall_cp <SND\dahall_cp>
2011-10-14 20:14:22 +0000

    - Updated version to 1.7.1

--- TaskEditor/Properties/AssemblyInfo.cs
+++ TaskEditor/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task Scheduler Editor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Task Scheduler Managed Wrapper UI Library")]
+[assembly: AssemblyDescription("Provides localizable UI elements for editing classes in the TaskScheduler library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodePlex Community")]
 [assembly: AssemblyProduct("TaskService")]
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.*")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
