64ce50150e25e7709fa2eaf4adab6270aef55dec
dahall_cp <SND\dahall_cp>
2011-12-27 16:43:32 +0000

    * Updated version to 1.7.2

--- TaskEditor/Properties/AssemblyInfo.cs
+++ TaskEditor/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodePlex Community")]
 [assembly: AssemblyProduct("TaskService")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.*")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
