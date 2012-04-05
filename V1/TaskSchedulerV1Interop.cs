8d3b95e30caafbf87fed47bc7ead0c71dd238f73
dahall_cp <SND\dahall_cp>
2012-04-05 21:41:42 +0000

    * Updated the SystemTime structure for interop so that it correctly converts DateTime.MinValue & MaxValue to the correct time. This is important for the Task.GetRunTimes method so you can pass in those DateTime values without an exception.

--- V1/TaskSchedulerV1Interop.cs
+++ V1/TaskSchedulerV1Interop.cs
@@ -338,11 +338,39 @@ namespace Microsoft.Win32.TaskScheduler.V1Interop
 			Milliseconds = Convert.ToUInt16(dt.Millisecond);
 		}
 
+		public SystemTime(ushort year, ushort month, ushort day, ushort hour = 0, ushort minute = 0, ushort second = 0, ushort millisecond = 0)
+		{
+			Year = year;
+			Month = month;
+			Day = day;
+			Hour = hour;
+			Minute = minute;
+			Second = second;
+			Milliseconds = millisecond;
+			DayOfWeek = 0;
+			DayOfWeek = Convert.ToUInt16(((DateTime)this).DayOfWeek);
+		}
+
 		public static implicit operator DateTime(SystemTime st)
 		{
-			if (st.Year == 0)
+			if (st.Year == 0 || st == MinValue)
 				return DateTime.MinValue;
+			if (st == MaxValue)
+				return DateTime.MaxValue;
 			return new DateTime(st.Year, st.Month, st.Day, st.Hour, st.Minute, st.Second, st.Milliseconds, DateTimeKind.Local);
+		}
+
+		public static bool operator ==(SystemTime s1, SystemTime s2)
+		{
+			return (s1.Year == s2.Year && s1.Month == s2.Month && s1.Day == s2.Day && s1.Hour == s2.Hour && s1.Minute == s2.Minute && s1.Second == s2.Second && s1.Milliseconds == s2.Milliseconds);
+		}
+
+		public static readonly SystemTime MinValue, MaxValue;
+
+		static public SystemTime()
+		{
+			MinValue = new SystemTime(1601, 1, 1);
+			MaxValue = new SystemTime(30827, 12, 31, 23, 59, 59, 999);
 		}
 	}
 
