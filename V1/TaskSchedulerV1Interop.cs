e921abcc76b54ffd11df2486c8dfa524e4700ef5
dahall_cp <SND\dahall_cp>
2012-04-12 05:35:13 +0000

    * Fixed compile errors resulting from changes in change set 77281

--- V1/TaskSchedulerV1Interop.cs
+++ V1/TaskSchedulerV1Interop.cs
@@ -348,7 +348,6 @@ namespace Microsoft.Win32.TaskScheduler.V1Interop
 			Second = second;
 			Milliseconds = millisecond;
 			DayOfWeek = 0;
-			DayOfWeek = Convert.ToUInt16(((DateTime)this).DayOfWeek);
 		}
 
 		public static implicit operator DateTime(SystemTime st)
@@ -365,12 +364,29 @@ namespace Microsoft.Win32.TaskScheduler.V1Interop
 			return (s1.Year == s2.Year && s1.Month == s2.Month && s1.Day == s2.Day && s1.Hour == s2.Hour && s1.Minute == s2.Minute && s1.Second == s2.Second && s1.Milliseconds == s2.Milliseconds);
 		}
 
+		public static bool operator !=(SystemTime s1, SystemTime s2)
+		{
+			return !(s1 == s2);
+		}
+
 		public static readonly SystemTime MinValue, MaxValue;
 
-		static public SystemTime()
+		static SystemTime()
 		{
 			MinValue = new SystemTime(1601, 1, 1);
 			MaxValue = new SystemTime(30827, 12, 31, 23, 59, 59, 999);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is SystemTime)
+				return ((SystemTime)obj) == this;
+			return base.Equals(obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
 		}
 	}
 
