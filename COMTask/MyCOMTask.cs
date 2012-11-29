0109b76462663af05bb559e51320caa836f704fd
dahall_cp <SND\dahall_cp>
2012-11-29 22:51:56 +0000

    * Some work on ComTaskHandler project (no success) * Fixed problem with tabs in TaskPropertiesControl * Updated to 1.9.2 to push out critical fix

--- COMTask/MyCOMTask.cs
+++ COMTask/MyCOMTask.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Timers;
-using Microsoft.Win32.TaskScheduler;
 
 namespace COMTask
 {
@@ -10,42 +9,40 @@ namespace COMTask
 	///  This task will write an entry to a log file every 5 seconds while active until 12 writes.
 	/// </summary>
 	[ComVisible(true), Guid("CE7D4428-8A77-4c5d-8A13-5CAB5D1EC734"), ClassInterface(ClassInterfaceType.None)]
-	public sealed class MyCOMTask : ITaskHandler
+	public class MyCOMTask : TaskHandlerBase
 	{
-		private ITaskHandlerStatus handlerService;
 		private Timer timer;
 		private DateTime lastWriteTime = DateTime.MinValue;
 		private byte writeCount = 0;
 		private const string file = @"C:\TaskLog.txt";
 
-		void ITaskHandler.Start(object pHandlerServices, string Data)
+		public MyCOMTask()
 		{
-			handlerService = pHandlerServices as ITaskHandlerStatus;
+			timer = new Timer(5000) { AutoReset = true };
+			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+		}
+
+		public override void Start(string data)
+		{
 			lastWriteTime = DateTime.Now;
 			timer_Elapsed(null, null);
 			timer.Enabled = true;
 		}
 
-		void ITaskHandler.Stop(out int pRetCode)
+		public override int Stop()
 		{
 			timer.Enabled = false;
-			pRetCode = 0;
+			return 0;
 		}
 
-		void ITaskHandler.Pause()
+		public override void Pause()
 		{
 			timer.Enabled = false;
 		}
 
-		void ITaskHandler.Resume()
+		public override void Resume()
 		{
 			timer.Enabled = true;
-		}
-
-		public MyCOMTask()
-		{
-			timer = new Timer(5000) { AutoReset = true };
-			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
 		}
 
 		void timer_Elapsed(object sender, ElapsedEventArgs e)
@@ -57,7 +54,7 @@ namespace COMTask
 					using (StreamWriter wri = File.AppendText(file))
 						wri.WriteLine("Log entry {0}", DateTime.Now);
 
-					handlerService.UpdateStatus((short)(++writeCount / 12), string.Format("Log file started at {0}", lastWriteTime));
+					this.StatusHandler.UpdateStatus((short)(++writeCount / 12), string.Format("Log file started at {0}", lastWriteTime));
 				}
 				catch { }
 			}
@@ -66,9 +63,8 @@ namespace COMTask
 			{
 				timer.Enabled = false;
 				writeCount = 0;
-				handlerService.TaskCompleted(0);
+				this.StatusHandler.TaskCompleted(0);
 			}
 		}
-
 	}
 }
