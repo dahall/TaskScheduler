53f9f682185b6ca2d06a1545e2ee9527e9d65e2a
dahall_cp <SND\dahall_cp>
2012-12-01 00:10:46 +0000

    * Figured out how to fix problems with COMTask example. The sample now will run under all versions of Windows, Vista and later. This is done by making it a COM+ server. Instruction for using it will be updated on the site.

--- COMTask/Properties/AssemblyInfo.cs
+++ COMTask/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.EnterpriseServices;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -34,3 +35,28 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.2.0")]
 [assembly: AssemblyFileVersion("1.9.2.0")]
+
+// The ActivationOption attribute indicates whether the component will be 
+// activated within the caller's process. You can set Activation.Option to 
+// Library or to Server.
+[assembly: ApplicationActivation(ActivationOption.Server)]
+
+// The ApplicationName attribute is the name that appears for the COM+  
+// application in the COM+ Catalog and the Component Services Administration 
+// console.
+[assembly: ApplicationName("My COM Task")]
+
+// Specify the application ID.
+//[assembly: ApplicationID("11F3EE74-29A6-4773-82C6-274A67961FB4")]
+
+// The Description attribute provides a description for the COM+ application 
+// in the COM+ Catalog and Component Services Administration console.
+[assembly: Description("COM Task for Sample ITaskHandler implementation.")]
+
+// COM+ security setting
+[assembly: ApplicationAccessControl(true,   //Authentication is on
+	AccessChecksLevel = AccessChecksLevelOption.ApplicationComponent,
+	Authentication = AuthenticationOption.Packet,
+	ImpersonationLevel = ImpersonationLevelOption.Identify)]
+
+[assembly: SecurityRole("Tester", SetEveryoneAccess = true)]
