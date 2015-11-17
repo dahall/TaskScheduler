979b0cb8268a0c50fb00290cc97d7a0ededf2f5c
dahall_cp <SND\dahall_cp>
2015-11-17 04:44:30 +0000

    * Version 2.5.4 for release

--- TaskService/Properties/SharedAssemblyInfo.cs
+++ TaskService/Properties/SharedAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCompany("CodePlex Community")]
 [assembly: AssemblyProduct("TaskService")]
 [assembly: AssemblyCopyright("Copyright © 2015")]
-[assembly: AssemblyVersion("2.5.3.0")]
-[assembly: AssemblyFileVersion("2.5.3.0")]
+[assembly: AssemblyVersion("2.5.4.0")]
+[assembly: AssemblyFileVersion("2.5.4.0")]
