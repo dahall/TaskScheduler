1d6752a168231fada852bfa61ae39c221d9ea418
dahall_cp <SND\dahall_cp>
2015-12-16 19:40:41 +0000

    * Version 2.5.7 * Fixed V1 error with ActionCollection.Clear * Cleaned versions out of resx files * Added TaskSchedulerConfig project (incomplete) to help identify connection problems on V1 and V2 systems

---
+++ TaskSchedulerConfig/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using Microsoft.Win32.TaskScheduler;
+
+namespace TaskSchedulerConfig
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var pArgs = new ProgramArguments();
+			if (Parser.ParseArgumentsWithUsage(args, pArgs))
+			{
+				string svr;
+				bool hasV2 = TaskService.Instance.HighestSupportedVersion > new Version(1, 1);
+
+				if (!string.IsNullOrEmpty(pArgs.server))
+				{
+					svr = pArgs.server;
+					Console.WriteLine($"Connecting to {pArgs.server}...");
+				}
+				else
+				{
+					svr = "(local)";
+				}
+
+				var vldr = new Validator(pArgs.server);
+
+				if (pArgs.report)
+				{
+					var r = new Reporter(vldr);
+					Console.WriteLine();
+					Console.WriteLine($"Configuration report for {svr}:");
+					Console.WriteLine($"========================================");
+					Console.WriteLine();
+					Console.WriteLine($"General Information");
+					Console.WriteLine($"-------------------");
+					ConsoleWriteLines(r.General());
+					Console.WriteLine();
+					Console.WriteLine($"Version 1 (XP/Server 2000)");
+					Console.WriteLine($"--------------------------");
+					ConsoleWriteLines(r.V1());
+					if (hasV2)
+					{
+						Console.WriteLine();
+						Console.WriteLine($"Version 2 (Vista/2003 and later)");
+						Console.WriteLine($"--------------------------------");
+						ConsoleWriteLines(r.V2());
+					}
+				}
+
+				if (pArgs.fix)
+				{
+					Console.WriteLine($"Repairing {svr}...");
+				}
+			}
+
+			Console.ReadKey();
+		}
+
+		static void ConsoleWriteLines(string[] lines)
+		{
+			foreach (string s in lines)
+				Console.WriteLine(s);
+		}
+
+		class ProgramArguments
+		{
+			//[Argument(ArgumentType.AtMostOnce, HelpText = "Name of remote server to check. Omitting this value will check the local machine.")]
+			public string server = null;
+
+		    //[Argument(ArgumentType.AtMostOnce, HelpText="Specifies that a repair of broken functions should be attempted.", DefaultValue = false)]
+		    public bool fix = false;
+
+		    [DefaultArgument(ArgumentType.AtMostOnce, HelpText="Specifies that a report should be shown. If no other options are shown, this is the default.", DefaultValue = true)]
+		    public bool report = true;
+		}
+	}
+}
---
+++ TaskSchedulerConfig/Validator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Permissions;
+using System.Security.Principal;
+
+namespace TaskSchedulerConfig
+{
+	class Validator
+	{
+		private string svr;
+		private WindowsIdentity id;
+		private SecurityIdentifier sid;
+		private WindowsPrincipal prin;
+
+		public Validator(string svr)
+		{
+			this.svr = svr;
+			oldFw = Environment.OSVersion.Version.Major == 5;
+			id = WindowsIdentity.GetCurrent();
+			sid = new SecurityIdentifier(id.User.Value);
+			prin = new WindowsPrincipal(id);
+		}
+
+		public bool UserIsAdmin => prin.IsInRole(WindowsBuiltInRole.Administrator);
+		public bool UserIsBackupOperator => prin.IsInRole(WindowsBuiltInRole.BackupOperator);
+		public bool UserIsServerOperator => prin.IsInRole(WindowsBuiltInRole.SystemOperator);
+		public bool V1TaskPathAccess
+		{
+			get
+			{
+				try
+				{
+					new FileIOPermission(FileIOPermissionAccess.AllAccess, Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Tasks")).Demand();
+					return true;
+				}
+				catch { }
+				return false;
+			}
+		}
+
+		public bool IsFirewallEnabled
+		{
+			get
+			{
+				if (oldFw)
+					return Firewall.LocalPolicy.CurrentProfile.FirewallEnabled;
+				else
+				{
+					const int NET_FW_PROFILE2_DOMAIN = 1;
+					const int NET_FW_PROFILE2_PRIVATE = 2;
+					const int NET_FW_PROFILE2_PUBLIC = 4;
+
+					bool result = false;
+					int CurrentProfiles = Firewall.CurrentProfileTypes;
+
+					// The returned 'CurrentProfiles' bit mask can have more than 1 bit set if multiple profiles are active or current at the same time
+					if ((CurrentProfiles & NET_FW_PROFILE2_DOMAIN) != 0 && Firewall.FirewallEnabled(NET_FW_PROFILE2_DOMAIN))
+						result = true;
+					if ((CurrentProfiles & NET_FW_PROFILE2_PRIVATE) != 0 && Firewall.FirewallEnabled(NET_FW_PROFILE2_PRIVATE))
+						result = true;
+					if ((CurrentProfiles & NET_FW_PROFILE2_PUBLIC) != 0 && Firewall.FirewallEnabled(NET_FW_PROFILE2_PUBLIC))
+						result = true;
+					return result;
+				}
+			}
+		}
+
+		private dynamic firewall;
+		private bool oldFw;
+
+		private dynamic Firewall
+		{
+			get
+			{
+				if (firewall == null)
+				{
+					Type NetFwMgrType = Type.GetTypeFromProgID(oldFw ? "HNetCfg.FwMgr" : "HNetCfg.FwPolicy2", svr, false);
+					firewall = Activator.CreateInstance(NetFwMgrType);
+				}
+				return firewall;
+			}
+		}
+
+		public Dictionary<string, bool> FirewallConfiguration
+		{
+			get
+			{
+				var ret = new Dictionary<string, bool>();
+				if (IsFirewallEnabled)
+				{
+					if (oldFw)
+					{
+						const int NET_FW_SERVICE_FILE_AND_PRINT = 0;
+						//const int NET_FW_SERVICE_UPNP = 1;
+						//const int NET_FW_SERVICE_REMOTE_DESKTOP = 2;
+						ret.Add("File and Printer Sharing", Firewall.LocalPolicy.CurrentProfile.Services.Item(NET_FW_SERVICE_FILE_AND_PRINT).Enabled);
+					}
+					else
+					{
+						ret.Add("Remote Scheduled Tasks Management", Firewall.IsRuleGroupCurrentlyEnabled("Remote Scheduled Tasks Management"));
+					}
+				}
+				return ret;
+			}
+		}
+
+		public string User => id.Name;
+	}
+}
