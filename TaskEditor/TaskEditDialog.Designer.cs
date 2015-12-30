e58ce4a300c85614c26e38f56f228660818aa20c
dahall_cp <SND\dahall_cp>
2015-12-30 16:08:02 +0000

    * Bug fixes to TaskEditDialog and TriggerEditDialog

--- TaskEditor/TaskEditDialog.Designer.cs
+++ TaskEditor/TaskEditDialog.Designer.cs
@@ -43,6 +43,7 @@
 			// cancelBtn
 			// 
 			resources.ApplyResources(this.cancelBtn, "cancelBtn");
+			this.cancelBtn.CausesValidation = false;
 			this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.cancelBtn.Name = "cancelBtn";
 			this.cancelBtn.UseVisualStyleBackColor = true;
