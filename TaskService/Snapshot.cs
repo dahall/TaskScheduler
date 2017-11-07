d56913cd3201c324fd504ea28427242dd22c9f3e
David Hall <dahall68@gmail.com>
2017-11-06 17:24:20 -0700

    Final API changes to TaskSchedulerSnapshot before release (cleanup, sealing classes, method/prop naming, etc.)

--- TaskService/Snapshot.cs
+++ TaskService/Snapshot.cs
@@ -1,23 +1,50 @@
 ﻿#if !(NET20 || NET35 || NET40)
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
+using System.Xml;
+using System.Xml.Schema;
 using System.Xml.Serialization;
 
 namespace Microsoft.Win32.TaskScheduler
 {
+	/// <summary>Abstract class representing a secured item for storage in a <see cref="TaskSchedulerSnapshot"/>.</summary>
+	public abstract class SnapshotItem
+	{
+		/// <summary>Initializes a new instance of the <see cref="SnapshotItem"/> class.</summary>
+		/// <param name="path">The path to the item.</param>
+		/// <param name="sddl">The SDDL for the item.</param>
+		internal SnapshotItem(string path, string sddl) { Path = path; Sddl = sddl; }
+
+		/// <summary>Gets the path to the item.</summary>
+		/// <value>The path to the item.</value>
+		public string Path { get; private set; }
+
+		/// <summary>Gets the SDDL for the item.</summary>
+		/// <value>The SDDL for the item.</value>
+		public string Sddl { get; private set; }
+	}
+
+	/// <summary>Represents a <see cref="TaskFolder"/> instance and captures its name and security.</summary>
+	public sealed class TaskFolderSnapshot : SnapshotItem
+	{
+		/// <summary>Initializes a new instance of the <see cref="TaskFolderSnapshot"/> class.</summary>
+		/// <param name="path">The path to the item.</param>
+		/// <param name="sddl">The SDDL for the item.</param>
+		internal TaskFolderSnapshot(string path, string sddl) : base(path, sddl) { }
+	}
+
 	/// <summary>
 	/// Represents all the information about the tasks and folders from a <see cref="TaskService"/> instance that can be used to reconstitute tasks and folders
-	/// on the same or different systems.
-	/// <note>This class and related classes are only available under the .NET 4.5.2 build and later .NET versions due to dependencies on threading and compressed (zip) files.</note>
+	/// on the same or different systems. <note>This class and related classes are only available under the .NET 4.5.2 build and later .NET versions due to
+	/// dependencies on threading and compressed (zip) files.</note>
 	/// </summary>
-	[Serializable, XmlRoot(IsNullable = false)]
-	public class TaskSchedulerSnapshot
+	public sealed class TaskSchedulerSnapshot : IXmlSerializable
 	{
 		private const string hdrfile = ".metadata.xml";
 		private List<SnapshotItem> items;
@@ -45,8 +72,6 @@ namespace Microsoft.Win32.TaskScheduler
 		/// <summary>
 		/// Gets a list of <see cref="TaskSnapshot"/> and <see cref="TaskFolderSnapshot"/> instances the represent the tasks and folders from a Task Scheduler instance.
 		/// </summary>
-		[XmlElement("Task", typeof(TaskSnapshot))]
-		[XmlElement("TaskFolder", typeof(TaskFolderSnapshot))]
 		public List<SnapshotItem> Items
 		{
 			get => items ?? (items = (Path == null ? new List<SnapshotItem>() : GetArchiveItems(Path)));
@@ -54,8 +79,15 @@ namespace Microsoft.Win32.TaskScheduler
 		}
 
 		/// <summary>Gets the path of the file based snapshot.</summary>
-		[XmlIgnore]
 		public string Path { get; private set; }
+
+		/// <summary>Gets the machine name of the server from which the snapshot was taken.</summary>
+		/// <value>The target server name.</value>
+		public string TargetServer { get; private set; }
+
+		/// <summary>Gets the UTC time stamp for when the snapshot was taken.</summary>
+		/// <value>The time stamp.</value>
+		public DateTime TimeStamp { get; private set; } = DateTime.UtcNow;
 
 		/// <summary>
 		/// Creates a compressed zip file that contains all the information accessible to the user from the <see cref="TaskService"/> instance necessary to
@@ -94,10 +126,18 @@ namespace Microsoft.Win32.TaskScheduler
 
 		/// <summary>Register a list of snapshot items (tasks and folders) into the specified Task Scheduler.</summary>
 		/// <param name="ts">The <see cref="TaskService"/> into which the tasks and folders are registered.</param>
-		/// <param name="itemPaths">The list of paths representing the tasks and folders from this snapshot that should be registered on the <see cref="TaskService"/> instance.</param>
-		/// <param name="applyAccessRights">If <c>true</c>, takes the access rights from the snapshot item and applies it to both new and existing tasks and folders.</param>
-		/// <param name="overwriteExisting">If <c>true</c>, overwrite any existing tasks and folders found in the target Task Scheduler that match the path of the snapshot item.</param>
-		/// <param name="passwords">Lookup table for password. Provide pairs of the user/group account name and the associated passwords for any task that requires a password.</param>
+		/// <param name="itemPaths">
+		/// The list of paths representing the tasks and folders from this snapshot that should be registered on the <see cref="TaskService"/> instance.
+		/// </param>
+		/// <param name="applyAccessRights">
+		/// If <c>true</c>, takes the access rights from the snapshot item and applies it to both new and existing tasks and folders.
+		/// </param>
+		/// <param name="overwriteExisting">
+		/// If <c>true</c>, overwrite any existing tasks and folders found in the target Task Scheduler that match the path of the snapshot item.
+		/// </param>
+		/// <param name="passwords">
+		/// Lookup table for password. Provide pairs of the user/group account name and the associated passwords for any task that requires a password.
+		/// </param>
 		/// <param name="cancelToken">A cancellation token to use to cancel this asynchronous operation.</param>
 		/// <param name="progress">An optional <see cref="IProgress{T}"/> instance to use to report progress of the asynchronous operation.</param>
 		/// <returns>An asynchronous <see cref="Task"/> instance.</returns>
@@ -107,15 +147,24 @@ namespace Microsoft.Win32.TaskScheduler
 			if (itemPaths == null) throw new ArgumentNullException(nameof(itemPaths));
 			var items = Items.Where(i => i is TaskSnapshot).Join(itemPaths, a => a.Path, b => b, (a, b) => a).ToList();
 			if (items.Count != itemPaths.Count()) throw new ArgumentException($"Unable to locate matching tasks to all values of {nameof(itemPaths)}.", nameof(itemPaths));
-			await System.Threading.Tasks.Task.Run(() => InternalExtractAndRegister(ts, items, applyAccessRights, overwriteExisting, passwords, cancelToken, progress));
+			await System.Threading.Tasks.Task.Run(() => InternalRestore(ts, items, applyAccessRights, overwriteExisting, passwords, cancelToken, progress));
 		}
 
 		/// <summary>Register a list of snapshot items (tasks and folders) into the specified Task Scheduler.</summary>
 		/// <param name="ts">The <see cref="TaskService"/> into which the tasks and folders are registered.</param>
-		/// <param name="items">The list of <see cref="SnapshotItem"/> instances representing the tasks and folders from this snapshot that should be registered on the <see cref="TaskService"/> instance.</param>
-		/// <param name="applyAccessRights">If <c>true</c>, takes the access rights from the snapshot item and applies it to both new and existing tasks and folders.</param>
-		/// <param name="overwriteExisting">If <c>true</c>, overwrite any existing tasks and folders found in the target Task Scheduler that match the path of the snapshot item.</param>
-		/// <param name="passwords">Lookup table for password. Provide pairs of the user/group account name and the associated passwords for any task that requires a password.</param>
+		/// <param name="items">
+		/// The list of <see cref="SnapshotItem"/> instances representing the tasks and folders from this snapshot that should be registered on the
+		/// <see cref="TaskService"/> instance.
+		/// </param>
+		/// <param name="applyAccessRights">
+		/// If <c>true</c>, takes the access rights from the snapshot item and applies it to both new and existing tasks and folders.
+		/// </param>
+		/// <param name="overwriteExisting">
+		/// If <c>true</c>, overwrite any existing tasks and folders found in the target Task Scheduler that match the path of the snapshot item.
+		/// </param>
+		/// <param name="passwords">
+		/// Lookup table for password. Provide pairs of the user/group account name and the associated passwords for any task that requires a password.
+		/// </param>
 		/// <param name="cancelToken">A cancellation token to use to cancel this asynchronous operation.</param>
 		/// <param name="progress">An optional <see cref="IProgress{T}"/> instance to use to report progress of the asynchronous operation.</param>
 		/// <returns>An asynchronous <see cref="Task"/> instance.</returns>
@@ -123,19 +172,78 @@ namespace Microsoft.Win32.TaskScheduler
 		{
 			if (ts == null) throw new ArgumentNullException(nameof(ts));
 			if (items == null) throw new ArgumentNullException(nameof(items));
-			await System.Threading.Tasks.Task.Run(() => InternalExtractAndRegister(ts, items, applyAccessRights, overwriteExisting, passwords, cancelToken, progress));
+			await System.Threading.Tasks.Task.Run(() => InternalRestore(ts, items, applyAccessRights, overwriteExisting, passwords, cancelToken, progress));
 		}
 
 		/// <summary>Register a list of snapshot items (tasks and folders) into the specified Task Scheduler.</summary>
 		/// <param name="ts">The <see cref="TaskService"/> into which the tasks and folders are registered.</param>
-		/// <param name="items">The list of <see cref="SnapshotItem"/> instances representing the tasks and folders from this snapshot that should be registered on the <see cref="TaskService"/> instance.</param>
-		/// <param name="applyAccessRights">If <c>true</c>, takes the access rights from the snapshot item and applies it to both new and existing tasks and folders.</param>
-		/// <param name="overwriteExisting">If <c>true</c>, overwrite any existing tasks and folders found in the target Task Scheduler that match the path of the snapshot item.</param>
-		/// <param name="passwords">Lookup table for password. Provide pairs of the user/group account name and the associated passwords for any task that requires a password.</param>
+		/// <param name="items">
+		/// The list of <see cref="SnapshotItem"/> instances representing the tasks and folders from this snapshot that should be registered on the
+		/// <see cref="TaskService"/> instance.
+		/// </param>
+		/// <param name="applyAccessRights">
+		/// If <c>true</c>, takes the access rights from the snapshot item and applies it to both new and existing tasks and folders.
+		/// </param>
+		/// <param name="overwriteExisting">
+		/// If <c>true</c>, overwrite any existing tasks and folders found in the target Task Scheduler that match the path of the snapshot item.
+		/// </param>
+		/// <param name="passwords">
+		/// Lookup table for password. Provide pairs of the user/group account name and the associated passwords for any task that requires a password.
+		/// </param>
 		public void Restore(TaskService ts, ICollection<SnapshotItem> items, bool applyAccessRights, bool overwriteExisting, IDictionary<string, string> passwords)
 		{
 			var c = new System.Threading.CancellationTokenSource();
-			InternalExtractAndRegister(ts, items, applyAccessRights, overwriteExisting, passwords, c.Token, null);
+			InternalRestore(ts, items, applyAccessRights, overwriteExisting, passwords, c.Token, null);
+		}
+
+		XmlSchema IXmlSerializable.GetSchema() => null;
+
+		void IXmlSerializable.ReadXml(XmlReader reader)
+		{
+			reader.MoveToContent();
+			if (DateTime.TryParseExact(reader.GetAttribute("timestamp"), "o", CultureInfo.CurrentCulture.DateTimeFormat, DateTimeStyles.AssumeUniversal, out DateTime dt))
+				TimeStamp = dt;
+			TargetServer = reader.GetAttribute("machine");
+			reader.GetAttribute("version");
+			while (reader.Read())
+			{
+				if (reader.Name == "Task")
+				{
+					var tsnap = new TaskSnapshot(reader.GetAttribute("path"), reader.GetAttribute("sddl"), true);
+					var e = reader.GetAttribute("enabled");
+					if (e != null && e == "false") tsnap.Enabled = false;
+					Items.Add(tsnap);
+				}
+				else if (reader.Name == "TaskFolder")
+					Items.Add(new TaskFolderSnapshot(reader.GetAttribute("path"), reader.GetAttribute("sddl")));
+			}
+		}
+
+		void IXmlSerializable.WriteXml(XmlWriter writer)
+		{
+			writer.WriteAttributeString("timestamp", TimeStamp.ToString("o"));
+			if (TargetServer != null)
+				writer.WriteAttributeString("machine", TargetServer);
+			writer.WriteAttributeString("version", "1.0");
+			foreach (var i in items)
+			{
+				if (i is TaskSnapshot t)
+				{
+					writer.WriteStartElement("Task");
+					writer.WriteAttributeString("path", t.Path);
+					writer.WriteAttributeString("sddl", t.Sddl);
+					if (!t.Enabled)
+						writer.WriteAttributeString("enabled", "false");
+					writer.WriteEndElement();
+				}
+				else if (i is TaskFolderSnapshot f)
+				{
+					writer.WriteStartElement("TaskFolder");
+					writer.WriteAttributeString("path", f.Path);
+					writer.WriteAttributeString("sddl", f.Sddl);
+					writer.WriteEndElement();
+				}
+			}
 		}
 
 		private static List<SnapshotItem> GetArchiveItems(string archiveFile)
@@ -168,10 +276,10 @@ namespace Microsoft.Win32.TaskScheduler
 			using (var zip = new ZipArchive(zipstr, ZipArchiveMode.Create))
 			{
 				GetCount(ts.RootFolder);
-				var snapshot = new TaskSchedulerSnapshot();
+				var snapshot = new TaskSchedulerSnapshot { TargetServer = ts.TargetServer ?? Environment.MachineName };
 				GetContents(ts.RootFolder, snapshot.Items, zip);
 				snapshot.Items.Sort((t1, t2) => String.Compare(t1.Path, t2.Path, StringComparison.InvariantCultureIgnoreCase));
-				using (var hdr = new StreamWriter(zip.CreateEntry(hdrfile).Open()))
+				using (var hdr = new StreamWriter(zip.CreateEntry(hdrfile).Open(), Encoding.UTF8))
 					new XmlSerializer(snapshot.GetType()).Serialize(hdr, snapshot);
 				snapshot.Path = path;
 				return snapshot;
@@ -193,7 +301,7 @@ namespace Microsoft.Win32.TaskScheduler
 				foreach (var t in f.Tasks)
 				{
 					list.Add(new TaskSnapshot(t.Path, t.GetSecurityDescriptorSddlForm(siall), t.Enabled, t.Xml));
-					using (var wr = new StreamWriter(zip.CreateEntry(t.Path.TrimStart('\\') + ".xml").Open()))
+					using (var wr = new StreamWriter(zip.CreateEntry(t.Path.TrimStart('\\') + ".xml").Open(), Encoding.Unicode))
 						wr.Write(t.Xml);
 					cancelToken.ThrowIfCancellationRequested();
 					progress?.Report(new Tuple<int, string>(++i * 100 / count, t.Path));
@@ -209,22 +317,14 @@ namespace Microsoft.Win32.TaskScheduler
 			}
 		}
 
-		private void InternalExtractAndRegister(TaskService ts, ICollection<SnapshotItem> items, bool applyAccessRights, bool overwriteExisting, IDictionary<string, string> passwords, System.Threading.CancellationToken cancelToken, IProgress<Tuple<int, string>> progress)
+		private void InternalRestore(TaskService ts, ICollection<SnapshotItem> items, bool applyAccessRights, bool overwriteExisting, IDictionary<string, string> passwords, System.Threading.CancellationToken cancelToken, IProgress<Tuple<int, string>> progress)
 		{
 			var i = 0;
 			progress?.Report(new Tuple<int, string>(0, ""));
 			foreach (var item in items)
 			{
 				cancelToken.ThrowIfCancellationRequested();
-				if (item is TaskFolderSnapshot f)
-				{
-					var sf = ts.GetFolder(f.Path);
-					if (sf == null)
-						sf = EnsureFolder(f.Path);
-					else if (overwriteExisting && applyAccessRights)
-						sf.SetSecurityDescriptorSddlForm(f.Sddl);
-				}
-				else if (item is TaskSnapshot t)
+				if (item is TaskSnapshot t)
 				{
 					var td = ts.NewTask();
 					td.XmlText = t.TaskDefinitionXml;
@@ -239,6 +339,14 @@ namespace Microsoft.Win32.TaskScheduler
 						st = st.Folder.RegisterTaskDefinition(System.IO.Path.GetFileName(t.Path), td, TaskCreation.CreateOrUpdate, td.Principal.ToString(), pwd, td.Principal.LogonType, applyAccessRights ? t.Sddl : null);
 						if (!t.Enabled) st.Enabled = false;
 					}
+				}
+				else if (item is TaskFolderSnapshot f)
+				{
+					var sf = ts.GetFolder(f.Path);
+					if (sf == null)
+						sf = EnsureFolder(f.Path);
+					else if (overwriteExisting && applyAccessRights)
+						sf.SetSecurityDescriptorSddlForm(f.Sddl);
 				}
 				progress?.Report(new Tuple<int, string>(++i * 100 / items.Count, item.Path));
 			}
@@ -279,49 +387,9 @@ namespace Microsoft.Win32.TaskScheduler
 		}
 	}
 
-	/// <summary>Represents a <see cref="TaskFolder"/> instance and captures its name and security.</summary>
-	[Serializable, XmlType]
-	public class TaskFolderSnapshot : SnapshotItem
-	{
-		/// <summary>Initializes a new instance of the <see cref="TaskFolderSnapshot"/> class.</summary>
-		public TaskFolderSnapshot() { }
-
-		/// <summary>Initializes a new instance of the <see cref="TaskFolderSnapshot"/> class.</summary>
-		/// <param name="path">The path to the item.</param>
-		/// <param name="sddl">The SDDL for the item.</param>
-		internal TaskFolderSnapshot(string path, string sddl) : base(path, sddl) { }
-	}
-
-	/// <summary>Abstract class representing a secured item for storage in a <see cref="TaskSchedulerSnapshot"/>.</summary>
-	[Serializable, XmlInclude(typeof(TaskFolderSnapshot)), XmlInclude(typeof(TaskSnapshot)), XmlType]
-	public abstract class SnapshotItem
-	{
-		/// <summary>Initializes a new instance of the <see cref="SnapshotItem"/> class.</summary>
-		public SnapshotItem() { }
-
-		/// <summary>Initializes a new instance of the <see cref="SnapshotItem"/> class.</summary>
-		/// <param name="path">The path to the item.</param>
-		/// <param name="sddl">The SDDL for the item.</param>
-		internal SnapshotItem(string path, string sddl) { Path = path; Sddl = sddl; }
-
-		/// <summary>Gets or sets the path to the item.</summary>
-		/// <value>The path to the item.</value>
-		[XmlAttribute("path")]
-		public string Path { get; set; }
-
-		/// <summary>Gets or sets the SDDL for the item.</summary>
-		/// <value>The SDDL for the item.</value>
-		[XmlAttribute("sddl")]
-		public string Sddl { get; set; }
-	}
-
 	/// <summary>Represents a <see cref="Task"/> instance and captures its details.</summary>
-	[Serializable, XmlType]
-	public class TaskSnapshot : SnapshotItem
+	public sealed class TaskSnapshot : SnapshotItem
 	{
-		/// <summary>Initializes a new instance of the <see cref="TaskSnapshot"/> class.</summary>
-		public TaskSnapshot() { }
-
 		/// <summary>Initializes a new instance of the <see cref="TaskSnapshot"/> class.</summary>
 		/// <param name="path">The path to the item.</param>
 		/// <param name="sddl">The SDDL for the item.</param>
@@ -329,15 +397,13 @@ namespace Microsoft.Win32.TaskScheduler
 		/// <param name="xml">The XML for the <see cref="TaskDefinition"/>.</param>
 		internal TaskSnapshot(string path, string sddl, bool enabled, string xml = null) : base(path, sddl) { Enabled = enabled; TaskDefinitionXml = xml; }
 
-		/// <summary>Gets or sets a value indicating whether th <see cref="Task"/> is enabled.</summary>
+		/// <summary>Gets a value indicating whether th <see cref="Task"/> is enabled.</summary>
 		/// <value><c>true</c> if enabled; otherwise, <c>false</c>.</value>
-		[XmlAttribute("enabled"), DefaultValue(true)]
-		public bool Enabled { get; set; } = true;
+		public bool Enabled { get; internal set; } = true;
 
-		/// <summary>Gets or sets the <see cref="TaskDefinition"/> XML.</summary>
+		/// <summary>Gets the <see cref="TaskDefinition"/> XML.</summary>
 		/// <value>The <see cref="TaskDefinition"/> XML.</value>
-		[XmlIgnore]
-		public string TaskDefinitionXml { get; set; }
+		public string TaskDefinitionXml { get; internal set; }
 	}
 }
 #endif
