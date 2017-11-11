7afb212e559055566169a2d9620cc500d993bb42
David Hall <dahall68@gmail.com>
2017-11-10 21:14:55 -0700

    Created new TaskSchedulerMockup project to clean up testing environment in TestTaskService project

---
+++ TaskSchedulerMockup/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TaskSchedulerMockup
+{
+	static class Program
+	{
+		/// <summary>The main entry point for the application.</summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new TSMMCMockup());
+		}
+	}
+}
