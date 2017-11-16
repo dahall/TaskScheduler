b8ec14e9bcbc4ec2e96c76d94cce52472e56125b
David Hall <dahall68@gmail.com>
2017-11-16 13:08:33 -0700

    Updated file versions to 2.7.2

--- VBTestTaskService/My Project/AssemblyInfo.vb
+++ VBTestTaskService/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.7.1.0")> 
-<Assembly: AssemblyFileVersion("2.7.1.0")> 
+<Assembly: AssemblyVersion("2.7.2.0")> 
+<Assembly: AssemblyFileVersion("2.7.2.0")> 
