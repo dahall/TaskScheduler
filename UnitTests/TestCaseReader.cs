d1136a553b6014f355cfcd66c37dde96e9da503a
David Hall <dahall@byu.net>
2019-01-02 08:16:21 -0700

    Added UWP and UnitTest projects

---
+++ UnitTests/TestCaseReader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NUnit.Framework
+{
+	public class TestCaseReader
+	{
+		public static IEnumerable FromFile(string path)
+		{
+			string[] args = null;
+			int iname = -1, ireturns = -1;
+			foreach (var line in System.IO.File.ReadAllLines(path))
+			{
+				if (string.IsNullOrEmpty(line)) continue;
+				var values = new List<string>(line.Split('\t'));
+				if (args == null)
+					ProcessHeader(values);
+				else
+					yield return Make(values);
+			}
+
+			void ProcessHeader(List<string> items)
+			{
+				iname = items.IndexOf("Name");
+				ireturns = items.IndexOf("Returns");
+				if (iname >= 0) items.RemoveAt(iname);
+				if (ireturns >= 0) { ireturns = iname >= 0 && iname < ireturns ? ireturns - 1 : ireturns; items.RemoveAt(ireturns); }
+				args = items.ToArray();
+			}
+
+			TestCaseData Make(List<string> values)
+			{
+				string nameVal = null, retVal = null;
+				if (iname >= 0) { nameVal = values[iname]; values.RemoveAt(iname); }
+				if (ireturns >= 0) { retVal = values[ireturns]; values.RemoveAt(ireturns); }
+				var objs = values.Cast<object>().ToArray();
+				for (var i = 0; i < objs.Length; i++)
+					if (objs[i] is string s) { if (s == "") objs[i] = null; else if (s == "TRUE") objs[i] = false; else if (s == "FALSE") objs[i] = false; }
+				var tcd = new TestCaseData(objs);
+				if (!(nameVal is null)) tcd.SetName(nameVal);
+				if (!(retVal is null)) tcd.Returns(retVal);
+				//return nameVal is null ? tcd.SetArgDisplayNames(args) : tcd;
+				return tcd;
+			}
+		}
+	}
+}
---
+++ UnitTests/TestClass.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Win32.TaskScheduler.Tests
+{
+	[TestFixture]
+	public class TestTaskService
+	{
+		[Test]
+		public void TestCtor()
+		{
+			Assert.That(() => new TaskService(Server, User, Domain, Pwd, true).Dispose(), Throws.Nothing);
+		}
+
+		[TestCaseSource(typeof(TestCaseReader), "FromFile", new object[] { @"C:\Temp\ServerConnectionTestCases.txt" })]
+		public void TestCtorV1(string Server, string User, string Domain, string Pwd, bool UserIsAdmin, bool ValidSvr, bool ValidCred)
+		{
+			try
+			{
+				new TaskService(Server, User, Domain, Pwd, true).Dispose();
+			}
+			catch (Exception e)
+			{
+				if (ValidCred && ValidSvr) Assert.Fail(e.ToString());
+			}
+		}
+
+		[TestCaseSource(typeof(TestCaseReader), "FromFile", new object[] { @"C:\Temp\ServerConnectionTestCases.txt" })]
+		public void TestCtorV2(string Server, string User, string Domain, string Pwd, bool UserIsAdmin, bool ValidSvr, bool ValidCred)
+		{
+			try
+			{
+				new TaskService(Server, User, Domain, Pwd, false).Dispose();
+			}
+			catch (Exception e)
+			{
+				if (ValidCred && ValidSvr) Assert.Fail(e.ToString());
+			}
+		}
+
+		[Test]
+		public void TestInstance()
+		{
+			Assert.That(TaskService.Instance, Is.Not.Null);
+			Assert.That(TaskService.Instance.Connected, Is.True);
+		}
+
+		[Test]
+		public void TestLibraryVersion()
+		{
+			switch (Environment.OSVersion.Version.Major)
+			{
+				case 5:
+					Assert.That(TaskService.LibraryVersion.Minor, Is.EqualTo(1));
+					break;
+				case 6:
+					switch (Environment.OSVersion.Version.Minor)
+					{
+						case 0:
+							Assert.That(TaskService.LibraryVersion.Minor, Is.EqualTo(2));
+							break;
+						case 1:
+							Assert.That(TaskService.LibraryVersion.Minor, Is.EqualTo(3));
+							break;
+						case 2:
+							Assert.That(TaskService.LibraryVersion.Minor, Is.EqualTo(4));
+							break;
+						case 3:
+							var build = uint.Parse(Microsoft.Win32.Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "ReleaseId", "0").ToString());
+							if (build < 1703)
+								Assert.That(TaskService.LibraryVersion.Minor, Is.EqualTo(5));
+							else
+								Assert.That(TaskService.LibraryVersion.Minor, Is.EqualTo(6));
+							break;
+					}
+					break;
+			}
+		}
+	}
+}
