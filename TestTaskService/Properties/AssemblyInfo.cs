40e8ce110fd011c99b0eed48481b8ac096977adf
David Hall <dahall@byu.net>
2019-01-02 10:21:41 -0700

    Updated project versions to 2.8.7

--- TestTaskService/Properties/AssemblyInfo.cs
+++ TestTaskService/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("GitHub Community")]
 [assembly: AssemblyProduct("TaskService")]
 [assembly: AssemblyCopyright("Copyright © 2017")]
-[assembly: AssemblyVersion("2.8.6")]
-[assembly: AssemblyFileVersion("2.8.6")]
+[assembly: AssemblyVersion("2.8.7")]
+[assembly: AssemblyFileVersion("2.8.7")]
