82f325047860fc8d845334f0bfbcebc64f3ed26a
dahall <dahall@byu.net>
2020-08-31 19:35:19 -0600

    Changed version to 2.8.21 and fixed/updated all NuGet references (#852)

--- TaskEditor/Properties/AssemblyInfo.cs
+++ TaskEditor/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("GitHub Community")]
 [assembly: AssemblyProduct("TaskService")]
 [assembly: AssemblyCopyright("Copyright © 2020")]
-[assembly: AssemblyVersion("2.8.20")]
-[assembly: AssemblyFileVersion("2.8.20")]
+[assembly: AssemblyVersion("2.8.21")]
+[assembly: AssemblyFileVersion("2.8.21")]
